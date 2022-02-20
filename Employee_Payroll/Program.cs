2301b5169512a78f125c39b0fa7ec84918c11f8f
Aniketw1551 <aniketwaghmare976@gmail.com>
2022-02-20 13:51:41 +0530

    [Aniket] Adding program for creating connection with DB

---
+++ Employee_Payroll/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll
+{
+    internal class Program
+    {
+        public static void EmployeePayroll()
+        { 
+         EmployeeRepository employeeRepository = new EmployeeRepository();
+            employeeRepository.GetEmployeeRecords();
+        }
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Welcome to Employee Payroll program");
+        EmployeePayroll();
+        Console.ReadLine();
+        }
+    }
+}
